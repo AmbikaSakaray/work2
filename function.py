3b53f30fc55ea3ec6531e1534102fe75d389c7ba
ambikasakaray4@gmail.com <ambikasakaray4@gmail.com>
2026-01-12 14:49:55 +0530

    Add function.py

---
+++ function.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    return a + b
+
+result : add(1, 2)
+print(result)  
+
+##a,b are the parameters and 1,2 are the arguments..
+##We can even write it as(a=1, b=2)
+##There are 5 types of arguments:
+# 1.positional
+# 2.keyword
+# 3.default
+# 4.variable length
+# 5.variable keyword length.
+#These are the 5 types of arguments...
+
+
+
+#Functions are again classified as lambda, userdefined, builtin, etc.
+
+#No of arguments = no of parameters
+
+
+0 
